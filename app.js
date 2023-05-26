a3741de1610c493a043d0bde1e4d20afde62d1ab
areto18 <areto18@gmail.com>
2023-05-26 03:05:21 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+/**Ejemplo para ver
+ * const publicPath = path.resolve(__dirname. './public');
+ * app.use( express.static(publicPath) );
+ */
+
+app.use(express.static(path.join(__dirname, '/public'))); //setear la carpeta de archivos publicos
+
+
+app.listen(3004, function () {
+    console.log("Servidor corriendo");
+});
+
+
+ app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/index.html'));
+ })
